8ba20c8ca97ab41de7904627d1056370c9c755fe
LukasVVK <148755527+LukasVVK@users.noreply.github.com>
2023-10-28 12:06:59 +0300

    Update Header1.h

--- Header1.h
+++ Header1.h
@@ -1,19 +1,20 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <numeric>
 #include <iomanip>
+#include <stdexcept>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
-
-int minimum(int a, int b);
 
 class Student {
     string Name, Surname;
@@ -22,7 +23,7 @@ class Student {
     float Rez;
 
 public:
-    Student();
+    Student() : Exam(0), Rez(0.0) {}
     Student(string N, string S, vector<int> H, int E);
     Student(const Student& A);
     Student& operator=(const Student& A);
@@ -31,8 +32,12 @@ public:
     float Vid();
     float Med();
     void Result(char pas);
-    void printas();
+    void printas() const;
 
     friend std::istream& operator>>(std::istream& in, Student& S);
     friend std::ostream& operator<<(std::ostream& out, const Student& S);
+    friend bool operator<(const Student& a, const Student& b);
 };
+
+int minimum(int a, int b);
+void skaitytiIsFailo(const std::string& failoPavadinimas, std::vector<Student>& Grupe);
