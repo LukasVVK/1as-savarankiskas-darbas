85f5af4e44ced84a4b2bfa8a6047346c559e16b8
LukasVVK <148755527+LukasVVK@users.noreply.github.com>
2024-01-07 16:43:24 +0200

    Create deque2.cpp

---
+++ deque2.cpp
@@ -0,0 +1,45 @@
+#include "test.h"
+
+int main() {
+    Timer timer;
+    std::deque<Student> Grupe;
+    std::string failoPavadinimas;
+
+    cout << "Iveskite failo pavadinima: ";
+    getline(cin, failoPavadinimas);
+
+    try {
+        timer.reset();
+        skaitytiIsFailo(failoPavadinimas, Grupe);
+        cout << "Laikas, praleistas skaitymui is failo: " << timer.elapsed() << " ms" << endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << "Klaida nuskaitant duomenis is failo: " << e.what() << std::endl;
+        return 1;
+    }
+
+    timer.reset();
+    std::sort(Grupe.begin(), Grupe.end());
+    cout << "Laikas, praleistas rikiavimui: " << timer.elapsed() << " ms" << endl;
+
+    timer.reset();
+    std::deque<Student> vargsiukai, kietiakiai;
+    std::partition_copy(Grupe.begin(), Grupe.end(), std::back_inserter(vargsiukai), std::back_inserter(kietiakiai),
+        [](const Student& s) { return s.getRez() < 5.0; });
+    cout << "Laikas, praleistas grupiu atskyrimui: " << timer.elapsed() << " ms" << endl;
+
+    timer.reset();
+    std::ofstream vargsiukaiFile("vargsiukai.txt"), kietiakiaiFile("kietiakiai.txt");
+    if (!vargsiukaiFile || !kietiakiaiFile) {
+        std::cerr << "Nepavyko atidaryti isvesties failu." << std::endl;
+        return 1;
+    }
+    std::cout << "Laikas, praleistas failu kurimui: " << timer.elapsed() << " ms" << std::endl;
+    for (const auto& v : vargsiukai) vargsiukaiFile << v;
+    for (const auto& k : kietiakiai) kietiakiaiFile << k;
+    vargsiukaiFile.close();
+    kietiakiaiFile.close();
+    cout << "Laikas, praleistas rasant i failus: " << timer.elapsed() << " ms" << endl;
+
+    return 0;
+}
